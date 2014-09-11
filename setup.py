547fb7c26d0d94f02cdd4160d8671283748eb0d0
Don Brown <mrdon@twdata.org>
2014-09-10 23:08:27 -0600

    Up to 0.2-dev

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='AC-Flask-HipChat',
-    version='0.1.1',
+    version='0.2-dev',
     url='https://bitbucket.org/mrdon/ac-flask-hipchat',
     license='APLv2',
     author='Don Brown',
