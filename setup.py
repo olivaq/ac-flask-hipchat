a3e6108be79fde41dca7e78da7cf7a62f5bebbd1
Patrick Streule <pstreule@atlassian.com>
2016-08-17 00:46:53 +0000

    Back to development: 0.2.11

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='AC-Flask-HipChat',
-    version='0.2.10',
+    version='0.2.11.dev0',
     url='https://bitbucket.org/atlassianlabs/ac-flask-hipchat',
     license='APLv2',
     author='Don Brown',
